755f15dccde12c9b927c10c85a5c16b4d65e87f7
BrunoNaves07 <bruno.naves@estudante.ufla.br>
2020-08-27 16:53:15 -0300

    index.php

--- config/funcoes.php
+++ config/funcoes.php
@@ -45,7 +45,7 @@ class Funcoes {
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav mr-auto">
             <li class="nav-item active">
-              <a class="nav-link" href="#">Início <span class="sr-only">(current)</span></a>
+              <a class="nav-link" href="index.php">Início <span class="sr-only">(current)</span></a>
             </li>
             <li class="nav-item">
               <a class="nav-link" href="clientes.php">Clientes</a>
